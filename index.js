3564eb192ae41e2397f3414878bbb649ee017e70
tsyrdev <95694865+tsyrdev@users.noreply.github.com>
2023-03-07 11:57:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup"
+    },
+    touch: {
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchend"
+    }
+};
+
+let deviceType = "";
+
+let draw = false;
+let eraser = false;
+
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
